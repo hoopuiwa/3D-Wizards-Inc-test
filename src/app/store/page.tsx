38ef801b2579c984d193db1965c75ba83c064808
john-a-flinn <flinnj@hawaii.edu>
2024-11-15 04:15:05 -1000

    Store skeleton
    Only visual nothing does anything

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,53 +1,146 @@
-import { getServerSession } from 'next-auth';
-import { Col, Container, Row, Table } from 'react-bootstrap';
-import { prisma } from '@/lib/prisma';
-import StuffItem from '@/components/StuffItem';
-import { loggedInProtectedPage } from '@/lib/page-protection';
-import authOptions from '@/lib/authOptions';
+'use client';
 
-/** Render a list of stuff for the logged in user. */
-const ListPage = async () => {
-  // Protect the page, only logged in users can access it.
-  const session = await getServerSession(authOptions);
-  loggedInProtectedPage(
-    session as {
-      user: { email: string; id: string; randomKey: string };
-      // eslint-disable-next-line @typescript-eslint/comma-dangle
-    } | null,
-  );
-  const owner = (session && session.user && session.user.email) || '';
-  const stuff = await prisma.stuff.findMany({
-    where: {
-      owner,
-    },
-  });
-  // console.log(stuff);
+import { useState } from 'react';
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+/* Link to db later? */
+const products = [
+  {
+    id: 1,
+    name: 'Product a',
+    type: 'Product a',
+    colors: 2,
+    price: '$115',
+    image: '/images/3d-wizards-lowres.png',
+    bestSeller: true,
+  },
+  {
+    id: 2,
+    name: 'Product b',
+    type: 'Product b',
+    colors: 1,
+    price: '$200',
+    image: '/images/3d-wizards-lowres.png',
+    bestSeller: true,
+  },
+  {
+    id: 3,
+    name: 'Product c',
+    type: 'Product c',
+    colors: 2,
+    price: '$123',
+    image: '/images/3d-wizards-lowres.png',
+    bestSeller: false,
+  },
+  {
+    id: 4,
+    name: 'Product d',
+    type: 'Product d',
+    colors: 7,
+    price: '$115',
+    image: '/images/3d-wizards-lowres.png',
+    bestSeller: true,
+  },
+  {
+    id: 5,
+    name: 'Product e',
+    type: 'Product e',
+    colors: 4,
+    price: '$200',
+    image: '/images/3d-wizards-lowres.png',
+    bestSeller: true,
+  },
+  {
+    id: 6,
+    name: 'Product f',
+    type: 'Product f',
+    colors: 2,
+    price: '$123',
+    image: '/images/3d-wizards-lowres.png',
+    bestSeller: false,
+  },
+];
+
+const StorePage = () => {
+  const [pickupToday, setPickupToday] = useState(false);
+
   return (
-    <main>
-      <Container id="list" fluid className="py-3">
-        <Row>
-          <Col>
-            <h1>Stuff</h1>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Quantity</th>
-                  <th>Condition</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {stuff.map((item) => (
-                  <StuffItem key={item.id} {...item} />
-                ))}
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-      </Container>
-    </main>
+    <Container fluid>
+      <Row>
+        {/* Sidebar */}
+        <Col md={3} className="border-end">
+          <h4>Pick Up Inperson</h4>
+          <Form.Check
+            type="switch"
+            id="pickup-today-switch"
+            label=""
+            checked={pickupToday}
+            onChange={() => setPickupToday(!pickupToday)}
+          />
+          <h5 className="mt-4">Categories</h5>
+          {/* add files to store each page later if time */}
+          <ul className="list-unstyled">
+            <li>Color</li>
+            <li>Size</li>
+            <li>Animal</li>
+            <li>etc</li>
+          </ul>
+          <h5 className="mt-4">Sale & Offers</h5>
+          {/* make link to product */}
+          <Form.Check label="Extra 25% off select styles" />
+          <Form.Check label="See Price in Bag" />
+          <Form.Check label="Sale" />
+          <h5 className="mt-4">Search</h5>
+          {/* make link to product */}
+          <form>
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Product Name"
+                aria-label="Search"
+              />
+              <button className="btn btn-dark" type="submit"> Search </button>
+            </div>
+          </form>
+        </Col>
+
+        {/* Product Grid */}
+        <Col md={9}>
+          <Row>
+            <Col className="d-flex justify-content-between align-items-center mb-3">
+              <h4>Shop Your Store (xyz)</h4>
+              <div>
+                {/* make link to product */}
+                <Button variant="link">Hide Filters</Button>
+                <Button variant="link">Sort By</Button>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            {products.map((product) => (
+              <Col md={4} key={product.id} className="mb-4">
+                <Card>
+                  <Card.Img variant="top" src={product.image} />
+                  <Card.Body>
+                    {product.bestSeller && (
+                      <Card.Text className="text-danger">Best Seller</Card.Text>
+                    )}
+                    <Card.Title>{product.name}</Card.Title>
+                    <Card.Text>{product.type}</Card.Text>
+                    <Card.Text>
+                      {product.colors}
+                      Colors
+                    </Card.Text>
+                    <Card.Text>{product.price}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
-export default ListPage;
+export default StorePage;
