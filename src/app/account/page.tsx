ed1ce2f7b6055a23e8ba4ba81934ec1da2b6d29f
hoopuiwa <puiwap1@gmail.com>
2024-11-26 20:23:09 -1000

    hah2
    hah2

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { loggedInProtectedPage } from '@/lib/page-protection';
-import { Row, Col, Container } from 'react-bootstrap';
+import AccountInfo from '@/components/AccountInfo';
 
 /** Account page */
 const AccountPage = async () => {
@@ -14,24 +14,7 @@ const AccountPage = async () => {
 
   return (
     <main>
-      <Container id="account-page" fluid className="py-3">
-        <Row className="justify-content-center align-items-center m-3">
-          <Col md={8}>
-            <h3 className="py-4">Cart:</h3>
-            <p>
-              Cart list items here:
-            </p>
-          </Col>
-        </Row>
-        <Row className="justify-content-center align-items-center m-3">
-          <Col md={8}>
-            <h3 className="py-4">Purchase History</h3>
-            <p>
-              Purchase history list here:
-            </p>
-          </Col>
-        </Row>
-      </Container>
+      <AccountInfo />
     </main>
   );
 };
---
+++ src/components/AccountInfo.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { redirect } from 'next/navigation';
+import LoadingSpinner from '@/components/LoadingSpinner';
+
+const AccountInfo: React.FC = () => {
+  const { data: session, status } = useSession();
+  // console.log('AddStuffForm', status, session);
+  const currentUser = session?.user?.email || '';
+  if (status === 'loading') {
+    return <LoadingSpinner />;
+  }
+  if (status === 'unauthenticated') {
+    redirect('/auth/signin');
+  }
+
+  return (
+    <Container id="account-page" fluid className="py-3">
+      <Row className="justify-content-center align-items-center m-3">
+        <Col md={8}>
+          <h3>Account: </h3>
+          {currentUser}
+        </Col>
+        <Col md={8}>
+          <h3 className="py-4">Cart:</h3>
+          <p>
+            Cart list items here:
+          </p>
+        </Col>
+      </Row>
+      <Row className="justify-content-center align-items-center m-3">
+        <Col md={8}>
+          <h3 className="py-4">Purchase History</h3>
+          <p>
+            Purchase history list here:
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default AccountInfo;
